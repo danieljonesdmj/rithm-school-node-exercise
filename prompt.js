de58bc625338e3c7eb07211485b1045d20e0544f
danieljonesdmj <danieljonesdmj@gmail.com>
2019-04-26 18:36:02 +0100

    Refactor

--- prompt.js
+++ prompt.js
@@ -5,43 +5,48 @@ const fs = require('fs');
 const request = require('request');
 const prompt = require('prompt');
 
-function getUserInput() {
-  prompt.start();
-  prompt.get(['searchterm'], (err, result) => {
-  let user_input = result.searchterm;
-  getURL(user_input);
-});
-};
-
-function getURL(keyword) {
-  let url = 'https://icanhazdadjoke.com/search?term=' + keyword;
-  requestJoke(url);
-};
-
-function requestJoke(url) {
-  request({
-    url: url,
-    json: true }, (error, response, body) => {
-      if (!error && response.statusCode == 200 && body.total_jokes === 0) {
-        console.log('...No joke found');
-      } else if  (!error && response.statusCode == 200) {
-        getRandomJoke(body.results[Math.floor(Math.random()*body.results.length)]);
-      };
-  });
-};
-  
-function getRandomJoke(json) {
-  let joke = json.joke;
-  console.log(joke);
-  addJokeToFile(joke);
-};
-
 function addJokeToFile(joke) {
   fs.appendFile('jokes.txt', `${joke}\n`, (err) => {
     if (err) throw err;
     console.log('...Joke added to jokes.txt');
   });
-};
+}
+
+function getRandomJoke(json) {
+  const { joke } = json;
+  console.log(joke);
+  addJokeToFile(joke);
+}
+
+function requestJoke(url) {
+  request({
+    url,
+    json: true,
+  }, (error, response, body) => {
+    if (!error && response.statusCode === 200 && body.total_jokes === 0) {
+      console.log('...No joke found');
+    } else if (!error && response.statusCode === 200) {
+      getRandomJoke(body.results[Math.floor(Math.random() * body.results.length)]);
+    }
+  });
+}
+
+function getURL(keyword) {
+  const url = `https://icanhazdadjoke.com/search?term=${keyword}`;
+  requestJoke(url);
+}
+
+function getUserInput() {
+  prompt.start();
+  prompt.get(['searchterm'], (err, result) => {
+    const userInput = result.searchterm;
+    if (userInput === '') {
+      console.log('...Finding random joke');
+      getURL(userInput);
+    } else {
+      getURL(userInput);
+    }
+  });
+}
 
 getUserInput();
-    

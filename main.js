a701ecdcbd6cc580361d1f1ed2950d70e59711ad
danieljonesdmj <danieljonesdmj@gmail.com>
2019-04-26 18:20:15 +0100

    Refactor

--- main.js
+++ main.js
@@ -2,33 +2,37 @@ const fs = require('fs');
 const process = require('process');
 const request = require('request');
 
-let keyword = process.argv[2];
-let url = 'https://icanhazdadjoke.com/search?term=' + keyword;
-
-function requestJoke() {
-  request({
-    url: url,
-    json: true }, (error, response, body) => {
-      if (!error && response.statusCode == 200 && body.total_jokes === 0) {
-        console.log('...No joke found');
-      } else if  (!error && response.statusCode == 200) {
-        getRandomJoke(body.results[Math.floor(Math.random()*body.results.length)]);
-      };
-  });
-};
-  
-function getRandomJoke(json) {
-  let joke = json.joke;
-  console.log(joke);
-  addJokeToFile(joke);
-};
+const keyword = process.argv[2];
+const url = `https://icanhazdadjoke.com/search?term=${keyword}`;
 
 function addJokeToFile(joke) {
   fs.appendFile('jokes.txt', `${joke}\n\n`, (err) => {
     if (err) throw err;
     console.log('...Joke added to jokes.txt');
   });
-};
+}
+
+function getRandomJoke(json) {
+  if (json && json.joke) {
+    const { joke } = json;
+    console.log(joke);
+    addJokeToFile(joke);
+  } else {
+    console.log('No joke found');
+  }
+}
+
+function requestJoke() {
+  request({
+    url,
+    json: true,
+  }, (error, response, body) => {
+    if (!error && response.statusCode === 200 && body.total_jokes === 0) {
+      console.log('...No joke found');
+    } else if (!error && response.statusCode === 200) {
+      getRandomJoke(body.results[Math.floor(Math.random() * body.results.length)]);
+    }
+  });
+}
 
 requestJoke(url);
-    
